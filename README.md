62114fbd01596fb06f03515917a7f310d2362058
Minh Quang Ngo <ngoquangminh3011@gmail.com>
2025-04-05 01:28:22 +0200

    update README and data.py; finish all queries

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 ## WRDS database set-up
 **Great resources to get started/ refer**:
-- (Retrieving historical members S&P500)[https://library.smu.edu.sg/topics-insights/notes-and-thoughts-retrieving-historical-members-sp-500-wrds]
+- [Retrieving historical members S&P500](https://library.smu.edu.sg/topics-insights/notes-and-thoughts-retrieving-historical-members-sp-500-wrds)
 - 
 **Steps**
 - Create a .pgpass file to store username and password
 - pip install wrds
+- Establish connection via `wrds.Connection()`
+- The easiest way to look at the data is to go to WRDS SAS Studio.
+- Get the content of any table: 
+```sql 
+proc contents data= crsp.dsf;
+run
+```
+
 **Notes:**
 - As of July 202, Compustat no longer has S&P Dow Jones Indices. Access it through CRSP.
 - For data after February 2025, you will need to change the data tables to their "*_V2" counterpart.
+
+## License and Usage
+© [Minh Quang Ngo] [2025]. All rights reserved.  
+This repository is provided for viewing and discussion purposes only. No license is granted for reuse, reproduction, or distribution of the code contained herein. Contact the author for permission regarding any use outside of this repository.
--- py/data.py
+++ py/data.py
@@ -1,7 +1,9 @@
 import wrds
+
 '''
 Importing all of the data in
 Period: 1990 -2018 (monthly)
+- All companies must stay within the 
 Stocks:
 [x] permno -> crsp a indexes.dsp500list v2
 [x] Returns
@@ -9,28 +11,36 @@ Stocks:
 
 
 Sector:
+[x] cmp.sector
+[] Sector names
 
 
 Liquidity risk factors
 [x] Turn over (turn) (vol/shrout)
 [x] Turn over volatility (SDTurn) (std. of turn)
 [x] log market equity (mvel1) (prc)
-[] dollar volume (dolvol)
+[x] dollar volume (dolvol) (#shares *price)
 [x] AMIHUD ILLQ: can get through "returns" and "volume"
-[] # zero trading days (zero trade)
-[] bid-ask spread (baspread)
+[x] # zero trading days (zero trade) ( zero trading day:vol =0)
+[x] bid-ask spread (baspread)
 
 Sentiment
 
-Factor variables
+Factor variables:
+[x] Excess return of a stock
+    [x]Risk free return
+[x] Market return 
+[x] Size factor( Small minus big)
+[x] Value factor (High minus low)
+[x] Momentum factor (MOM)
+[x] Profitability factor (Robust minus weak (RMW))
+[x] Investment factor (CMA)
 
-
+[] SAVE IT
 '''
 
 #Establish wrds connection
 db = wrds.Connection()
-db.list_libraries()
-db.list_tables(library='crsp')
 
 #Dates
 start_date = "1998-01-01"
@@ -39,29 +49,59 @@ end_date = "2018-12-31"
 #Stocks 
 '''
 Database:
-- crsp.dsf: stock date, permno, volume, return
-- crsp_a_indexes.dsp500list_v2: permno
+- crsp.dsf: permno, stock date, permno, volume, return
+- crsp.dsenames: permno, ticker, sector
+- crsp.ccmxpf_linktable : gvkey match with permno
+- comp.company: gsector
+
 '''
-stock = db.raw_sql(
+
+
+
+stock_ff_sector = db.raw_sql(
     f"""
-select 
-    stock.date, stock.permno, 
-    stock.vol, stock.ret,
-    stock.shrout, stock.prc
-from crsp.dsf as stock
-inner join (
-    select permno
-    from crsp_a_indexes.dsp500list_v2
-        where mbrenddt >= '{end_date}' or mbrenddt is null
-            and mbrstartdt <= '{start_date}'
-    group by permno
-) as constituents
-on stock.permno = constituents.permno
-where stock.date between '{start_date}' and '{end_date}'
-order by stock.date, stock.permno;
-""")
+    select
+        stock.date,
+        stock.permno,
+        names.ticker,
+        stock.vol,
+        stock.ret,
+        stock.shrout,
+        stock.prc,
+        stock.ask,
+        stock.bid,
+        cmp.gsector,
+        ff5.mktrf,
+        ff5.smb,
+        ff5.hml,
+        ff5.rmw,
+        ff5.umd,
+        ff5.cma,
+        ff5.rf
+    from crsp.dsf as stock
 
-stock.permno.unique()
+    inner join crsp_a_indexes.dsp500list_v2 as sp500
+        on stock.permno = sp500.permno
+        and stock.date between sp500.mbrstartdt and coalesce(sp500.mbrenddt, stock.date)
 
+    left join crsp.dsenames as names
+        on stock.permno = names.permno
+        and stock.date between names.namedt and coalesce(names.nameendt, stock.date)
+
+    left join crsp.ccmxpf_linktable as link -- query this in to get the gvkey, connecting stock with comp
+        on stock.permno = link.lpermno
+        and link.usedflag = 1 --primary link specific to the 2 joined 
+        and stock.date between link.linkdt and coalesce(link.linkenddt, stock.date)
+
+    left join comp.company as cmp on link.gvkey=cmp.gvkey 
+
+    left join ff.fivefactors_daily as ff5 on stock.date = ff5.date
+
+    where stock.date between '{start_date}' and '{end_date}'
+    order by stock.date, stock.permno;
+    """
+)
+
+stock_ff_sector['permno'].unique()
 #Close WRDS connection
 db.close()
