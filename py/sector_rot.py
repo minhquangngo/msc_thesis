f5245ac60c4fbe0e6204d15ee34b86e36b83c6b5
Minh Quang Ngo <ngoquangminh3011@gmail.com>
2025-06-17 21:59:33 +0200

    (BUG] fix

--- py/sector_rot.py
+++ py/sector_rot.py
@@ -359,10 +359,12 @@ class rolling_pred():
             hold_sect = int(condition_low_vr and condition_high_pred)
             signal.append(hold_sect)
         
-        sig_index = apriori_df.index[self.lookback_time+1 :] 
-        #TODO: take a look at sig_index
-        signal_series         = pd.Series(signal, index=sig_index.shift(1), name="signal") #[CHANGE]
-        rule_diagnostics_df   = pd.DataFrame(rule_book).set_index("idx").shift(1, axis=0) #[CHANGE]
+        sig_index = apriori_df.index[self.lookback_time+1 :]
+        signal_series = (
+            pd.Series(signal, index=sig_index, name="signal")  # build series
+            .shift(1)                                          # then lag the values
+        )
+        rule_diagnostics_df = pd.DataFrame(rule_book).set_index("idx").shift(1, axis=0)
             
         return signal_series, rule_diagnostics_df
 
